b18ed380929c5f8981174e1f29175c5e7d0595ef
jeongho.park1993 <jeongho.park1993@gmail.com>
2024-03-17 23:30:27 +0900

    Login 화면 kotlin으로 구현중

--- app/src/main/java/com/example/tp02/LoginActivity.kt
+++ app/src/main/java/com/example/tp02/LoginActivity.kt
@@ -2,11 +2,19 @@ package com.example.tp02
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.tp02.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        binding.loginActivity = this
+    }
+
+    //로그인 함수
+    fun clickLogin(i : String, j : String) {
 
 
     }
